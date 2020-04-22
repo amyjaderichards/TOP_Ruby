19822ff4ae959c7e2ba0bbc5755e3d5f76783018
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-22 11:42:24 +0100

    Stock Picker exercise for TOP Ruby course

---
+++ Stock_Picker/StockPicker.rb
@@ -0,0 +1,20 @@
+def stock_picker(arr)
+    selling_margin = arr.min - arr.max
+    best_days_to_buy_sell = []
+
+    arr.combination(2).to_a.map do |x|
+        if x[1]-x[0] > selling_margin
+            selling_margin = x[1] - x[0]
+            best_days_to_buy_sell = [arr.index(x[0]), arr.index(x[1])]
+        else
+            next
+        end
+    end
+best_days_to_buy_sell
+end
+
+p stock_picker([1, 2, 3, 4, 5, 6, 7]) #returns [0,6]
+p stock_picker([7, 6, 5, 4, 3, 2, 1]) #returns [0,1]
+p stock_picker([10, 1, 7, 9, 11, 17, 9]) #returns [1,5]
+p stock_picker([1, 2, 3, 6, 9, 1, 0]) #returns [0,4]
+p stock_picker([10, 1, 2, 3, 4, 5, 6]) #returns [1,6]
