b43d01df0a1ac1cb612d693f252b822d4d97faa6
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-22 11:14:11 +0100

    SubStrings exercise for TOP Ruby Course

---
+++ SubStrings/Substrings.rb
@@ -0,0 +1,17 @@
+def substrings(string, string_array)
+    string_counts = {}
+    string_array.each do |entry|
+        if string.downcase.include?(entry)
+            instances = string.downcase.scan(entry)
+            count = instances.count
+            string_counts[entry] = count
+        end
+    end
+    string_counts
+    end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+p substrings("low", dictionary)
+p substrings("go", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
