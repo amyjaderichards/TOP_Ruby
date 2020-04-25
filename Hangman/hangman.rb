e478b654804e4f074ec0741d067654429923178d
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-25 22:50:29 +0100

    Initial commit

---
+++ Hangman/hangman.rb
@@ -0,0 +1,14 @@
+begin
+    # this game requires the colorize gem
+    require 'colorize'
+rescue LoadError
+    puts 'This game works best if you install the "colorize" gem, please consider installing it'
+end
+
+require 'json'
+
+# require all files under the lib directory
+Dir[File.join(__dir__, 'lib', '*.rb')].each { |file| require file }
+
+game = Game.new
+game.play
