5782eb80d24cb7ee327cf0f5778b7d51cd9d8fe6
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-27 18:57:58 +0100

    Finished Merge Sort exercise

---
+++ MergeSort/merge_sort.rb
@@ -0,0 +1,22 @@
+def merge_sort(arr)
+    return list is list.size < 2
+
+    left = merge_sort(list[0..list.size / 2 - 1])
+    right = merge_sort(list[(list.size / 2)..-1])
+
+    merge(left, right)
+end
+
+
+def merge(left, right)
+    return right if left.empty?
+    return left if right.empty?
+    return [right[0]] + merge(right[1..-1], left) if right[0] <= left[0]
+
+    [left[0]] + merge(right, left[1..-1])
+end
+
+
+
+puts 'list: #{ary = rand(1..15).times.map { rand(50) }}'
+puts 'sorted list: #{merge_sort(arr)}'
